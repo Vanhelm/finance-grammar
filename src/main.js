3b50b31f54a4d92893216013e10d625c9646ffdb
Максим Похильчук <pohilchuk.m@roistat.com>
2022-02-18 14:50:29 +0300

    roistat v2

--- src/main.js
+++ src/main.js
@@ -10,7 +10,16 @@ import VueCountdown from '@chenfengyuan/vue-countdown';
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 
-
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '09c63c69eef3e820d892a236fb85de26');
+//Отключение ловца лидов - програмно
+window.onRoistatModuleLoaded = function () {
+    window.roistat.leadHunter.isEnabled = false; //отключить ловец лидов на странице
+};
 
 
 createApp(App).use(store).use(router).use(VueAxios, axios).use(VuePlyr, {
